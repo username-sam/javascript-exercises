3fecd095a9ddadfb56ad95c0a0e1f7683c650911
username-sam <218394446+username-sam@users.noreply.github.com>
2025-08-01 22:23:44 +0600

    6 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(s,n) {
 
+    let result = "";
+
+   for (let i = 1 ; i <= n ; i++){
+    result += s;
+   }
+   if(n < 0){
+    return "ERROR"
+  } else return result
 };
 
 // Do not edit below this line
