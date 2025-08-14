2ea4d3b69aeb3ee0a5572aad0d8fec8540eb1b11
username-sam <218394446+username-sam@users.noreply.github.com>
2025-08-15 01:58:16 +0600

    doing no.9

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...arg) {
+    let newArray = []
+    
+    arr.forEach(element => {
+
+
+        if(!arg.includes(element)){
+            newArray.push(element)
+        }
+    });
+
+    return newArray
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first,second) {
+    let start = first
+    let end  = second
+    let sum = 0;
 
+    if(first <= 0 || second <= 0){
+        return sum = 'ERROR'
+    }else if(isNaN(first) || isNaN(second)){
+        return sum = 'ERROR'
+    }
+    if (second < first){
+       start = second
+       end = first
+    }
+    
+    for(let i = start; i <= end ; i ++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
